a26e8f240c980517c7c6eeeaaba56823463a51ec
Alex Khost <akhost@gmail.com>
2025-05-31 22:41:12 -0400

    Fix: bind to 0.0.0.0 for Railway

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello World from Node.js!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
